699bd342f15f91e92d787b7a19cd2442f04a80ea
alexsander <alexsander@gmai.com>
2018-04-07 22:20:38 +0300

    Finish

---
+++ byNow.php
@@ -0,0 +1,15 @@
+<?php
+
+$mailheaders = "Content-type: text/html; charset=UTF-8 \r\n";
+$mailheaders .= "From: fire.co.ua <no-reply@fire.co.ua>";
+$mailheaders .= "Reply-To: fire.co.ua";
+$messeng ="<h1>Данные Пользователя</h1>
+<p>Имя пользователя:". $_POST["name"] . "</p>
+<p>Телефон пользователя:". $_POST["tel"] . "</p>
+<p>Выбор пользователя:". $_POST["select"] . "</p>
+
+";
+mail('anast.nosal@gmail.com',"Form Test site",$messeng,$mailheaders);
+header('Location: index.html');
+exit;
+?>
